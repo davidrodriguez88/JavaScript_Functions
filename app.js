65bfe79a2beed9625884b9f18ffee957d3c9c725
davidrodriguez88 <davidxrodriguez@me.com>
2026-03-13 13:26:04 -0400

    Completed Exercise 1

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count){
+    for (let i = 1; i <= count; i++) {
+        if (i % 2 != 0){
+            console.log(i)
+        }
+    }
+}
+printOdds(100);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
